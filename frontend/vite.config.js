7bc2abe9f8e745c7ccbe160b0c6f41153a9f5c1c
Mouli Vunnam <moulivunnam100@gmail.com>
2025-11-26 16:32:28 -0500

    Add backend proxy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://portfolio-backend-3jfv.onrender.com/',
         changeOrigin: true,
       }
     }
   }
 })
-
